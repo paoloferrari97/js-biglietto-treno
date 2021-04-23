12239d616621bd1b5a483c250136da61a2d00bb1
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-04-23 15:03:24 +0200

    Creo cartelle e file e inizio l'esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+//età passeggero
+var age = parseInt(prompt("Qual è la tua età?"));
+console.log(age);
+
+//chilometri da percorrere
+var km = parseInt(prompt("Quanti chilometri percorrerai?"));
+console.log(km);
+
+//prezzo biglietto
+var prezzo = 0.21 * km;
+console.log(prezzo);
+
+//se minorenni sconto 20%, se over 65 sconto 40%
+
+
+//prezzo finale con massimo 2 decimali
